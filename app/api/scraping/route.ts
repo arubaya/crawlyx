c4d2fa3b98f81696e71c57fc3e0d42b00660751f
arubaya <abi.arubaya@gmail.com>
2024-03-05 00:30:16 +0700

    try with @sparticuz/chromium-min

--- app/api/scraping/route.ts
+++ app/api/scraping/route.ts
@@ -1,11 +1,41 @@
 import { NextResponse } from "next/server";
 import puppeteer from "puppeteer";
+import chromium from "@sparticuz/chromium-min";
+
+chromium.setHeadlessMode = true;
+
+const getBrowser = async () => {
+  const browser = await puppeteer.launch({
+    args: process.env.NODE_ENV === "development" ? undefined : chromium.args,
+    defaultViewport:
+      process.env.NODE_ENV === "development"
+        ? undefined
+        : chromium.defaultViewport,
+    executablePath:
+      process.env.NODE_ENV === "development"
+        ? undefined
+        : await chromium.executablePath(
+            "https://github.com/Sparticuz/chromium/releases/download/v122.0.0/chromium-v122.0.0-pack.tar"
+          ),
+    headless: process.env.NODE_ENV === "development" ? false : true,
+  });
+  return browser;
+  // return process.env.NODE_ENV === "development"
+  //   ? await puppeteer.launch({ headless: false })
+  //   : await puppeteerCore.launch({
+  //       args: chromium.args,
+  //       defaultViewport: chromium.defaultViewport,
+  //       executablePath: await chromium.executablePath(
+  //         "https://github.com/Sparticuz/chromium/releases/download/v122.0.0/chromium-v122.0.0-pack.tar"
+  //       ),
+  //     });
+};
 
 export async function POST(request: Request) {
   try {
     const data = await request.json();
 
-    const browser = await puppeteer.launch();
+    const browser = await getBrowser();
     const page = await browser.newPage();
     await page.setViewport({ width: 1024, height: 738 });
 
